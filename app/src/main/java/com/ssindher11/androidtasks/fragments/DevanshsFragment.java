939dfd2fd1f4b2f69098f7d3e4b2a7cc388971c7
dev5151 <41675135+dev5151@users.noreply.github.com>
2019-04-28 21:12:07 +0530

    Update DevanshsFragment.java

--- app/src/main/java/com/ssindher11/androidtasks/fragments/DevanshsFragment.java
+++ app/src/main/java/com/ssindher11/androidtasks/fragments/DevanshsFragment.java
@@ -4,6 +4,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.airbnb.lottie.LottieAnimationView;
+import com.ssindher11.androidtasks.R;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,10 +16,37 @@ import androidx.fragment.app.Fragment;
 
 public class DevanshsFragment extends Fragment {
 
+    private Button animationbtn;
+    private EditText edt1;
+    private LottieAnimationView animationView;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-    //TODO : Welcome to your Fragment Devansh.
+
+        View view= inflater.inflate(R.layout.fragment_devanshs, container, false);
+
+        animationView=view.findViewById(R.id.animation_view);
+
+        animationbtn=view.findViewById(R.id.animationbtn);
+        edt1=view.findViewById(R.id.edt1);
+
+        edt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String spd=edt1.getText().toString();
+                Float speed=Float.parseFloat(spd);
+                animationView.setSpeed(speed);
+
+                animationView.setAnimation("77-im-thirsty.json");
+                animationView.loop(Boolean.parseBoolean("true"));
+                animationView.playAnimation();
+            }
+        });
+        return view;
+
+//TODO : Welcome to your Fragment Devansh.
         //TODO : Step 1, Inflate your layout file
         //TODO : Step 2, Assign views to your JAVA Objects (Binding view to code)
         //TODO : Step 3, Once you become familiar with Fragments, choose any animation from Lottie
@@ -22,6 +54,5 @@ public class DevanshsFragment extends Fragment {
         //TODO : Step 5, Put a start button in your fragment, pressing which starts the Animation
         //TODO : P.S. The library has already been integrated. Refer to Lottie documentations and start coding part
         //TODO : A sample Fragment has been worked for you to see how view bindings are done in Fragments.
-        return super.onCreateView(inflater, container, savedInstanceState);
     }
 }
